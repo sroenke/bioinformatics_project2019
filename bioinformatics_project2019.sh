b9a0765dddcc6ccd32f5962eb688c3c483d12904
Matthew Wang <mwang7@crcfe02.crc.nd.edu>
2019-10-14 20:28:20 -0400

    Code now makes .fasta files containing all mcrA and hsp70 reference sequences.

---
+++ bioinformatics_project2019.sh
@@ -0,0 +1,17 @@
+# This code determines which proteomes contain a general consensus for the mcrA
+# gene. It takes those which contain the gene and then determines which have
+# the HSP70 gene, and how many copies of the HSP70 gene each contains. The 
+# results are then listed in tabular form and a text document ranking the 
+# proteomes by HSP70 content is produced. 
+
+# Adjustments to the code may be necessary depending on the user's 
+# file and directory organization.
+
+# Usage: bash bioinformatics_project2019.sh <GENE_1> <GENE_2>
+# Example: bash bioinformatics_project2019.sh mcrA hsp70
+
+cat ref_sequences/$1*.fasta >> $1.fasta
+cat ref_sequences/$2*.fasta >> $2.fasta
+
+# The above lines make cumulative .fasta files containing all reference
+# sequences for the mcrA and hsp70 genes, respectively.
